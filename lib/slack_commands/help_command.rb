160ab03a9d4afcf25d305bf8c4ace24a216f7a4b
Kevin Kuchta <kevin@kevinkuchta.com>
2016-04-17 13:54:55 -0700

    Update bub help

--- lib/slack_commands/help_command.rb
+++ lib/slack_commands/help_command.rb
@@ -10,7 +10,7 @@ class HelpCommand < SlackCommand
 
 `bub info` – list all known staging boxes, along with who has them claimed and when that box has last visited (according to the server logs).
 
-`bub take sassy 3 hours` - claim the staging box named joyable-sassy for the next 3 hours. If you omit the time period, bub will assume you want the box for 1 hour.
+`bub take sassy 3 hours` - claim the staging box named joyable-sassy for the next 3 hours. If you omit the time period, bub will assume you want the box for 1 hour.  If you omit the box name, bub will give you the first available server.
 
 `bub release sassy` - releases your claim on joyable-sassy.  Omit the box name to release your claim on any box currently claimed by you.
 
