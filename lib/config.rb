e5c44f416508011baefe59b65ae9894d37190864
Kevin Kuchta <kevin@kevinkuchta.com>
2017-01-19 17:13:51 -0800

    remove cannoli

--- lib/config.rb
+++ lib/config.rb
@@ -1,6 +1,6 @@
 # TODO: make configurable
 HEROKU_APPS = %w()
-APTIBLE_APPS = %w(gyro burrito hotdog cannoli)
+APTIBLE_APPS = %w(gyro burrito hotdog)
 APPS = HEROKU_APPS + APTIBLE_APPS
 APP_VENDOR = "joyable"
 APP_PREFIX = "#{APP_VENDOR}-"
